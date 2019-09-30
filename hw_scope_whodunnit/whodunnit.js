7e91b944b5cb59aee56ab1682ef7d33b2a29d419
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-30 18:14:16 +0100

    episode 2

--- hw_scope_whodunnit/whodunnit.js
+++ hw_scope_whodunnit/whodunnit.js
@@ -1,16 +1,37 @@
 // #### Episode 1
 
+// ```js
+// // const scenario = {
+// //   murderer: 'Miss Scarlet',
+// //   room: 'Library',
+// //   weapon: 'Rope'
+// // };
+// //
+// // const declareMurderer = function() {
+// //   return `The murderer is ${scenario.murderer}.`;
+// // }
+// //
+// // const verdict = declareMurderer();
+// // console.log(verdict);
+// // "The murderer is Miss Scarlet" will b output as the object scenario is declared outside of any function and has global scope.
+// ```
 
-const scenario = {
-  murderer: 'Miss Scarlet',
-  room: 'Library',
-  weapon: 'Rope'
-};
+// #### Episode 2
+//
+// ```js
+// const murderer = 'Professor Plum';
+//
+// const changeMurderer = function() {
+//   murderer = 'Mrs. Peacock';
+// }
+//
+// const declareMurderer = function() {
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// changeMurderer();
+// const verdict = declareMurderer();
+// console.log(verdict);
+// ```
 
-const declareMurderer = function() {
-  return `The murderer is ${scenario.murderer}.`;
-}
-
-const verdict = declareMurderer();
-console.log(verdict);
-// "The murderer is Miss Scarlet" will b output as the object scenario is declared outside of any function and has global scope.
+// This will trigger an assignment error when it tries to reassign the constant murderer within the changeMurderer function.
