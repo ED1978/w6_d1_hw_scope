bd91c5f4ed3d0343ed8376269d48cfe9a95c9c14
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-30 18:24:48 +0100

    episode 3

--- hw_scope_whodunnit/whodunnit.js
+++ hw_scope_whodunnit/whodunnit.js
@@ -35,3 +35,24 @@
 // ```
 
 // This will trigger an assignment error when it tries to reassign the constant murderer within the changeMurderer function.
+
+
+// #### Episode 3
+//
+// ```js
+let murderer = 'Professor Plum';
+
+const declareMurderer = function() {
+  let murderer = 'Mrs. Peacock';
+  return `The murderer is ${murderer}.`;
+}
+
+const firstVerdict = declareMurderer();
+console.log('First Verdict: ', firstVerdict);
+
+const secondVerdict = `The murderer is ${murderer}.`;
+console.log('Second Verdict: ', secondVerdict);
+// ```
+// The first output will be "First Verdict: The murderer is Mrs. Peacock", as Mrs. Peacock is the value of let murderer declared locally within the declareMurderer function which is then called in the firstVerdict function.
+
+// The second output will be "Second Verdict: The murderer is Proffessor Plum", as the secondVerdict function accesses the let murderer variable which is defined outside of any function thus giving it global scope. 
