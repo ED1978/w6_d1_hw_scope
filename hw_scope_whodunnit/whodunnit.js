01cefa9d84015fa24debf26d25791aadf8be8fbf
EDownie <53910027+ED1978@users.noreply.github.com>
2019-09-30 18:42:50 +0100

    episode 4

--- hw_scope_whodunnit/whodunnit.js
+++ hw_scope_whodunnit/whodunnit.js
@@ -40,19 +40,40 @@
 // #### Episode 3
 //
 // ```js
-let murderer = 'Professor Plum';
-
-const declareMurderer = function() {
-  let murderer = 'Mrs. Peacock';
-  return `The murderer is ${murderer}.`;
-}
-
-const firstVerdict = declareMurderer();
-console.log('First Verdict: ', firstVerdict);
-
-const secondVerdict = `The murderer is ${murderer}.`;
-console.log('Second Verdict: ', secondVerdict);
+// let murderer = 'Professor Plum';
+//
+// const declareMurderer = function() {
+//   let murderer = 'Mrs. Peacock';
+//   return `The murderer is ${murderer}.`;
+// }
+//
+// const firstVerdict = declareMurderer();
+// console.log('First Verdict: ', firstVerdict);
+//
+// const secondVerdict = `The murderer is ${murderer}.`;
+// console.log('Second Verdict: ', secondVerdict);
 // ```
 // The first output will be "First Verdict: The murderer is Mrs. Peacock", as Mrs. Peacock is the value of let murderer declared locally within the declareMurderer function which is then called in the firstVerdict function.
 
-// The second output will be "Second Verdict: The murderer is Proffessor Plum", as the secondVerdict function accesses the let murderer variable which is defined outside of any function thus giving it global scope. 
+// The second output will be "Second Verdict: The murderer is Proffessor Plum", as the secondVerdict function accesses the let murderer variable which is defined outside of any function thus giving it global scope.
+
+
+// #### Episode 4
+//
+// ```js
+let suspectOne = 'Miss Scarlet';
+let suspectTwo = 'Professor Plum';
+let suspectThree = 'Mrs. Peacock';
+
+const declareAllSuspects = function() {
+  let suspectThree = 'Colonel Mustard';
+  return `The suspects are ${suspectOne}, ${suspectTwo}, ${suspectThree}.`;
+}
+
+const suspects = declareAllSuspects();
+console.log(suspects);
+console.log(`Suspect three is ${suspectThree}.`);
+// ```
+// Output 1 will be "The suspects are Miss Scarlet, Professor Plum Colonel Mustard."  This is because declareAllSuspects accesses thr locally defined suspectThree and the globally defined supects One and Two.
+
+// The second output will be "Suspect three is Mrs Peacock" because the console.log is acessing the globally defined variable suspectThree.
